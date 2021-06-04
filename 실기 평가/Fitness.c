7556c6c1aab3358dbfbc2bbdf19b82e61371fc81
choiyeonseong <am3251@naver.com>
2021-06-04 17:03:59 +0900

    0604 전체 출력으로 변경

--- 실기 평가/Fitness.c
+++ 실기 평가/Fitness.c
@@ -11,98 +11,97 @@
 #include <string.h>
 #include <windows.h>
 
-typedef struct {
+typedef struct 
+{
 	int num;
 	char name[20];
 	double weight;
 }Fitness;
 
-int count = 0;	// 회원수
+Fitness* ary[100];		// 포인터 배열 선언
+void total_number(int count);						// 전체 등록 회원 수 반환
+double average_weight(Fitness** pary, int count);	// 평균 체중 반환
+int max_weight(Fitness** pary, int count);			// 최대 체중 회원의 index 반환		
+void print_info(Fitness** pary, int index);			// 회원 정보 출력	
+void free_ary(Fitness** pary, int count);			// 동적 할당 영역 해제	
 
-Fitness* ary[100];// 포인터 배열 선언
-void total_number(int count);// 전체 등록 회원 수 반환
-double average_weight(Fitness** pary, int count);// 평균 체중 반환
-int max_weight(Fitness** pary, int count);	// 최대 체중 회원의 index 반환		
-void print_info(Fitness** pary, int index);		// 회원 정보 출력	
-void free_ary(Fitness** pary, int count);		// 동적 할당 영역 해제	
+void new_member(int num, char* name, double weight);	// 신규회원 추가
 
-void new_member();	// 신규회원 추가
+static int count = 0;	// 전체 회원수
 
-int main()
+int main() 
 {
+	// 입력받기 위한 임시 변수
+	int num;
+	char name[20];
+	double weight;
+
 	while (1)
 	{
-		int menu = 0;
+		printf("회원번호 : ");
+		scanf("%d", &num);
 
-		printf("1 : 회원등록\n");
-		printf("2 : 전체 회원 수\n");
-		printf("3 : 회원 평균 몸무게\n");
-		printf("4 : 최대 체중 회원 번호\n");
-		printf("5 : 전체 회원 정보\n");
-		printf("6 : 회원 전체 삭제\n");
-		printf("-1 : 프로그램 종료\n$ ");
-
-		scanf("%d", &menu);
-		if (menu < 0)
+		if (num < 0)	// 음수 입력하면 종료
 		{
-			printf("프로그램 종료\n");
-			exit(0);
+			printf("입력 종료\n");
+			break;
 		}
 		else
 		{
-			switch (menu)
+			printf("회원이름 : ");
+			scanf("%s", name);
+			printf("회원 몸무게 : ");
+			scanf("%lf", &weight);
+			putchar('\n');
+
+			new_member(num, name, weight);	// 신규 회원 추가
+
+			total_number(count);	// 총 회원 수
+
+			printf("평균 체중 : %.2lf\n", average_weight(ary, count));	// 평균 체중 반환
+
+			// 최대 체중 회원
+			int max_index = max_weight(ary, count);
+			printf("최고 체중 회원 : %s -> %.2lf\n", ary[max_index]->name,ary[max_index]->weight);		
+
+			// 전체 회원 정보 출력
+			for (int i = 0; i < count; i++)
 			{
-			case 1:
-				new_member();
-				break;
-			case 2:
-				total_number(count);
-				break;
-			case 3:
-				printf("평균 몸무게 : %.2lf\n", average_weight(ary, count));
-				break;
-			case 4:
-				printf("최대 체중 회원 번호 : %d\n", max_weight(ary, count));
-				break;
-			case 5:
-				for (int i = 0; i < count; i++)
-				{
-					print_info(ary, i);
-				}
-				break;
-			case 6:
-				free_ary(ary, count);
-				break;
-			default:
-				printf("정해진 값이 아닙니다.\n");
-				break;
+				print_info(ary, i);
 			}
+
+			printf("계속하려면 키보드 입력\n");
+			getchar();
+			getchar();
+			system("cls");
 		}
 	}
+	// 동적 할당 해제
+	free_ary(ary, count);
 }
 
+
 // 동적할당 영역 해제
-void free_ary(Fitness** pary, int count)
+void free_ary(Fitness** pary, int count) 
 {
 	for (int i = 0; i < count; i++)
 	{
 		free(pary[i]);
 	}
-	printf("동적 할당 해제\n");
+	printf("\n동적 할당 해제\n");
 }
 
 // 회원 정보 출력
-void print_info(Fitness** pary, int index)
+void print_info(Fitness** pary, int index) 
 {
 	printf("****************\n");
-	printf("회원번호 : %d\n", pary[index]->num);
-	printf("회원이름 : %s\n", pary[index]->name);
-	printf("회원몸무게 : %.2lf\n", pary[index]->weight);
-	printf("****************\n");
+	printf("회원 번호 : %d\n", pary[index]->num);
+	printf("회원 이름 : %s\n", pary[index]->name);
+	printf("회원 몸무게 : %.2lf\n", pary[index]->weight);
 }
 
 // 최대 체중 회원의 index반환
-int max_weight(Fitness** pary, int count)
+int max_weight(Fitness** pary, int count) 
 {
 	int max = 0;
 	for (int i = 1; i < count; i++)
@@ -112,11 +111,11 @@ int max_weight(Fitness** pary, int count)
 			max = i;
 		}
 	}
-	return pary[max]->num;
+	return max;
 }
 
 // 평균 체중 반환
-double average_weight(Fitness** pary, int count)
+double average_weight(Fitness** pary, int count) 
 {
 	double sum = 0;
 	for (int i = 0; i < count; i++)
@@ -127,33 +126,101 @@ double average_weight(Fitness** pary, int count)
 }
 
 // 전체 등록 회원 수 반환
-void total_number(int count)
+void total_number(int count) 
 {
 	printf("총 회원수 : %d\n", count);
 }
 
 // 신규 회원 추가
-void new_member()
+void new_member(int num, char* name, double weight) 
 {
-	int num;
-	char name[20];
-	double weight;
+	// 중복 확인
+	// 중복되면 회원 추가 되지 않음
+	for (int i = 0; i < count; i++)
+	{
+		if (ary[i]->num == num || strcmp(ary[i]->name, name) == 0) 
+		{
+			printf("회원 번호나 이름 중복\n");
+			return;
+		}
+	}
 
-	printf("회원 번호 : ");
-	scanf("%d", &num);
-	printf("회원 이름 : ");
-	scanf("%s", name);
-	printf("회원 몸무게 : ");
-	scanf("%lf", &weight);
-
-	ary[count] = malloc(sizeof(Fitness));
+	ary[count] = malloc(sizeof(Fitness));	// 동적 할당
 	if (ary[count] != NULL)
 	{
 		ary[count]->num = num;
 		strcpy(ary[count]->name, name);
 		ary[count]->weight = weight;
 
-		printf("%d %s %lf\n", ary[count]->num, ary[count]->name, ary[count]->weight);
 		count++;
 	}
 }
+
+//int main()
+//{
+//	int num;
+//	char name[20];
+//	double weight;
+//
+//	while (1)
+//	{
+//		int menu = 0;
+//
+//		printf("1 : 회원등록\n");
+//		printf("2 : 전체 회원 수\n");
+//		printf("3 : 회원 평균 몸무게\n");
+//		printf("4 : 최대 체중 회원 번호\n");
+//		printf("5 : 전체 회원 정보\n");
+//		printf("6 : 회원 전체 삭제\n");
+//		printf("-1 : 프로그램 종료\n$ ");
+//
+//		scanf("%d", &menu);
+//		if (menu < 0)
+//		{
+//			printf("프로그램 종료\n");
+//			exit(0);
+//		}
+//		else
+//		{
+//			switch (menu)
+//			{
+//			case 1:
+//				printf("회원번호 : ");
+//				scanf("%d", &num);
+//				printf("회원이름 : ");
+//				scanf("%s", name);
+//				printf("회원 몸무게 : ");
+//				scanf("%lf", &weight);
+//				new_member(num, name, weight);
+//
+//				break;
+//			case 2:
+//				total_number(count);
+//				break;
+//			case 3:
+//				system("cls");
+//
+//				printf("평균 몸무게 : %.2lf\n", average_weight(ary, count));
+//				break;
+//			case 4:
+//				system("cls");
+//
+//				printf("최대 체중 회원 번호 : %d\n", max_weight(ary, count));
+//				break;
+//			case 5:
+//				system("cls");
+//				for (int i = 0; i < count; i++)
+//				{
+//					print_info(ary, i);
+//				}
+//				break;
+//			case 6:
+//				free_ary(ary, count);
+//				break;
+//			default:
+//				printf("정해진 값이 아닙니다.\n");
+//				break;
+//			}
+//		}
+//	}
+//}
